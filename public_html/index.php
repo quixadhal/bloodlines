3a55994a9974b68969cee49b1acc6d163140346c
WileyMUD IV <bloodlines@shadowlord.org>
2011-03-18 02:26:20 -0400

    Web page up

--- public_html/index.php
+++ public_html/index.php
@@ -1,93 +1,152 @@
+<?php
+$time_start = microtime(true);
+//$now = date('g:ia \o\n l, \t\h\e jS \o\f F, Y');
+?>
 <html>
-<head>
-<title> WileyMUD IV, the sickness of Dread Quixadhal. </title>
-<link rev="made" href="mailto:quixadhal@shadowlord.org">
-</head>
-<body background="gfx/marble_background.jpg" bgcolor="#505050" text="#d0d0d0" link="#ffffbf" vlink="#ffa040">
-<table border=0 cellspacing=5 cellpadding=5 width=100%>
-<tr> <td>
-<a href="http://www.shadowlord.org:8088/~wiley/linux.txt">
-<img src="gfx/linux95.jpg" border=0 align=center width=111 height=80
- alt="(Linux95!)">
-</a> </td>
-<td align=center>
-<center>
-  <a href="mailto:quixadhal@shadowlord.org">Dread Quixadhal</a> welcomes you to<br>
-<a href="telnet://wiley.shadowlord.org:3000/">
-WileyMUD III!<br>
-</a></center>
- </td>
-<td align=right>
-<a href="telnet://wiley.shadowlord.org:3000/">
-<img src="gfx/orc.jpg" border=0 align=center width=116 height=163
- alt="(telnet)">
-</a></td> 
-</tr>
-</table>
-<p></p>
-<p>As you finish your last sip of dark Nestharian Ale, the clash of steel
-interrupts your dreamy repose yet again.  Running to the door, you see some
-poor besotted elf trying to cast as the villager he just tried to rob brings
-a thick wooden club down on his head.</p>
-
-<p>"Get out of MY town you bastard!"  The sorry excuse that is Shylar's
-only cityguard runs up and starts beating on the villager... blissfully
-unaware that the fool elf started the fight in the first place.  You know
-that after 12 days of playing time, you could walk out and slaughter ALMOST
-everything in the room... but it's more fun to watch the newbie get pummeled
-as Eli starts healing the villager.</p>
-
-<p>WileyMUD is a venerable DikuMUD, whose origins lie nearly six years in
-the past.  Once upon a time, a wizard named Cyric took his brand new DikuMUD
-code and began building a world to run in it.  He wisely eschewed ALL the
-boring areas that came with the game, believing that only pathetic players
-would enjoy playing a game that looked just like everyone else's game.  He
-gathered such powerful names as Muthaarin'akya, Harlequin, Grimwell,
-Derkhil, Elcid, Daisy, and even Dark Muidnar.  With their help, he managed
-to terrorize the students of WMU for nearly three years with the evil
-concoction known as WileyMUD II. </p>
-
-<p>After a time, Cyric grew bored with his creation.  He had acquired new
-and more devious things known as "work", and "jobs".  As the ancient sun 3/60
-gasped its last breath, so too did WileyMUD fade away.  For a time, hopes
-held that WileyMUD III would be done, but the wretched "job" saw to it that
-this never came to be.</p>
-
-<p>Then Dread Quixadhal, Dark Lord of VI and player of WileyMUD II,
-discovered a way of summoning the shades of the dead.  With a mighty blast
-of power, he travelled back in time and wrested a copy of the Source from
-Cyric's treasure horde!  A battle ensued, and some of the World was damaged
-in the scuffle.  However, he dragged Source, World, and Vile Muidnar back
-into the present, and thus began the writing of WileyMUD III.  The addition
-of Sedna and Dirk has helped to balance out our team, and occasional visits
-by Highlander and Zar go mostly unnoticed.</p>
-
-<p>We have a relatively quiet mud here.  We have only recently started to
-advertise our presence again, because only now have I decided that the game
-is stable and finished enough to show to any but the most dedicated.  We were
-once known as one of the hardest, yet well balanced, DikuMUD's on the planet.
-After trying several other muds recently, I have to say this is still true.
-So, if you would like to see an ALL original world that is NOT a cakewalk...
-Please stop on by!</p>
-
-<table border=0 cellspacing=5 cellpadding=5 align="center" width=80%>
-  <tr>
-    <td width="30%" align="center">
-      <a href="http://www.shadowlord.org:8088/~wiley/wiley.txt">
-        <img src="gfx/clouds.gif" border=0 align="center" width=177 height=115 alt="(docs)">
-        <br />
-        Building information
-      </a> IS available!
-    </td>
-    <td width="40%"> &nbsp; </td>
-    <td width="30%" align="center">
-      <a href="gitweb.cgi?p=.git;a=summary">
-        <img src="/gitweb/git-logo.png" border=0 align="center" width=72 height=27 alt="(git)">
-        <br />
-        GIT Repository
-      </a>
-    </td>
-  </tr>
-</table>
-</body>
+    <head>
+        <meta http-equiv="refresh" content="300">
+        <script type="text/javascript">
+        function startClock()
+        {
+            var today=new Date();
+            var p="am";
+            var h=today.getHours();
+            var m=today.getMinutes();
+            var s=today.getSeconds();
+            var d=today.getDate();
+            var M=today.getMonth();
+            var y=today.getFullYear();
+            var month=new Array(12);
+            month[0]="January";
+            month[1]="February";
+            month[2]="March";
+            month[3]="April";
+            month[4]="May";
+            month[5]="June";
+            month[6]="July";
+            month[7]="August";
+            month[8]="September";
+            month[9]="October";
+            month[10]="November";
+            month[11]="December";
+            if( h > 11 ) {
+                p = "pm";
+                h = h - 12;
+                if( h == 0 ) {
+                    h = 12;
+                }
+            }
+            m=zeroPad(m);
+            s=zeroPad(s);
+            document.getElementById('clock').innerHTML=h+":"+m+":"+s+" "+p;
+            document.getElementById('date').innerHTML=month[M]+" "+d+", "+y;
+            t=setTimeout('startClock()',500);
+        }
+        function zeroPad(i)
+        {
+            if (i<10)
+              {
+                  i="0" + i;
+              }
+            return i;
+        }
+        </script>
+        <title>
+            Bloodlines:  WileyMUD IV
+        </title>
+        <link rev="made" href="mailto:quixadhal@shadowlord.org">
+    </head>
+    <body background="gfx/dark_wood.jpg" bgcolor="#505050" text="#d0d0d0" link="#ffffbf" vlink="#ffa040" onload="startClock()">
+        <table border=0 cellspacing=0 cellpadding=0 width=80% align="center">
+            <tr>
+                <td align="left" valign="top">
+                    <a href="telnet://wiley.shadowlord.org:3000/">
+                        <img src="gfx/bloodlines.png" border=0 width=469 height=160 alt="(Bloodlines:)">
+                    </a>
+                </td>
+                <td align="right" valign="bottom">
+                    <a href="telnet://wiley.shadowlord.org:3000/">
+                        <img src="gfx/wileymud4.png" border=0 width=354 height=81 alt="(WileyMUD IV)">
+                    </a>
+                </td>
+            </tr>
+        </table>
+        <table border=0 cellspacing=0 cellpadding=0 width=100% align="center">
+            <tr>
+                <td align="center" width="20%" valign="bottom">
+                    <a href="gitweb.cgi?p=.git;a=summary">
+                        <img src="/gitweb/git-logo.png" border=0 align="center" width=72 height=27 alt="(Git)">
+                    </a>
+                    <br />
+                    <br />
+                    <div id="clock" align="center" style="color: #d0d000;"><?php echo date('g:i:s a'); ?></div>
+                    <div id="date" align="center" style="color: #d0d000;"><?php echo date('F j, Y'); ?></div>
+                    <br />
+                    <a href="http://www.wunderground.com/US/MI/Kalamazoo.html?bannertypeclick=wu_blueglass">
+                        <img src="http://weathersticker.wunderground.com/cgi-bin/banner/ban/wxBanner?bannertype=wu_blueglass&airportcode=KAZO&ForcedCity=Kalamazoo&ForcedState=MI" alt="Click for Kalamazoo, Michigan Forecast" height="68" width="120" border=0 />
+                    </a>
+                </td>
+                <td align="center" width="60%">
+                    <table border=0 cellspacing=5 cellpadding=5 width=100% align="center">
+                        <tr>
+                            <td align="left" valign="top">
+                                <h2>
+                                    The story so far...
+                                </h2>
+                                It's been 371 years since Cyric the Destroyer tried to
+                                send the world into the void.  Mad Quixadhal saved us
+                                from utter annihilation, only to plunge us into a
+                                darkness that lasted for decades.
+                                <p>
+                                Finally, there is a new dawn.
+                                <p>
+                                The ancient empire of Nesthar has once again been
+                                swallowed by the jungle.  Gredth, ever a home to
+                                pirates and ruffians, was overrun by terrors from the
+                                depths of the ocean.  Mighty Highstaff has fallen, and
+                                is now home to feral halflings.  The humble village of
+                                Shylar remains, and it has become the only real bastion
+                                of civilization left in this part of the world.
+                                <p>
+                                Blood runs deep here.  The sons and daughters of
+                                nobility try to reclaim their birthright, while the
+                                children of heroes assert their claims by force.
+                                Wealthy merchants fight to buy what could not be
+                                purchased in the old world.
+                                <p>
+                                Mages have siezed control of ancient places of power.
+                                Temples rise to give the priests strongholds to counter
+                                them.  The laws of the old empire are ignored, and the
+                                rule of the sword has come.  Many choose to live in the
+                                shadows of these places, quietly gathering what they
+                                need to control them.
+                                <p>
+                                Do you have what it takes to walk between the lines?
+                                To carve a place for yourself in history?  Can you
+                                found a new empire here?  Or will you become fodder for
+                                another?
+                                <p>
+                                Welcome!  May your stay here be... interesting.
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td align="right" width="20%" valign="bottom">
+                    <a href="i3log.php">
+                        <img src="gfx/intermud_logs.png" border=0 align="right" width=154 height=200 alt="(Intermud)">
+                    </a>
+                </td>
+            </tr>
+        </table>
+        <table border=0 cellspacing=0 cellpadding=0 width=100% align="center">
+            <tr>
+                <!-- <td align="left" width="50%"><span style="color: #333333">Last refreshed at <? echo $now; ?>.&nbsp;</span></td> -->
+                <?php
+                $time_end = microtime(true);
+                $time_spent = $time_end - $time_start;
+                ?>
+                <td align="right" width="50%"><span style="color: #333333">&nbsp;Page generated in <? printf( "%8.4f", $time_spent); ?> seconds.</span></td>
+            </tr>
+       </table>
+    </body>
 </html>
