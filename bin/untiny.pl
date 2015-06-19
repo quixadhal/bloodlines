55c9a628b1385378ae302b19c35f02b0d7062569
WileyMUD IV <bloodlines@lenin.awasteoftime.net>
2015-06-18 21:40:17 -0400

    Updated to be more robust and look nicer.

--- bin/untiny.pl
+++ bin/untiny.pl
@@ -3,41 +3,77 @@
 
 # Fed a URL, this script will fetch it and return information about the actual
 # destination URL and, in the case of youtube, the video in question.
-
+# A second argument is for the mud channel the URL came from, if any.
+#
 # <link rel="canonical" href="/watch?v=IleiqUDYpFQ">
 # <link rel="alternate" media="handheld" href="http://m.youtube.com/watch?desktop_uri=%2Fwatch%3Fv%3DIleiqUDYpFQ&amp;v=IleiqUDYpFQ&amp;gl=US">
 # <meta name="title" content="Ronald Reagan: First Inaugural Address (1 of 3)">
 # <meta name="description" content="Senator Hatfield, Mr. Chief Justice, Mr. President, Vice President Bush, Vice President Mondale, Senator Baker, Speaker O\'Neill, Reverend Moomaw, and my fell...">
 # <meta name="keywords" content="president, ronald, reagan, first, inaugural, inauguration, address, washington, 80s, reaganomics, ronny, gipper, carter, jfk, kennedy, bush, clinton, republi...">
-
+# <meta itemprop="duration" content="PT8M26S">
+#
 # Here are the triggers I used in Tinyfugue, to feed it.
 #
-#; 00:45 <intergossip> Kalinash@Fire and Ice: http://www.youtube.com/watch?v=IleiqUDYpFQ
+# 00:45 <intergossip> Kalinash@Fire and Ice: http://www.youtube.com/watch?v=IleiqUDYpFQ
 #
-#/def -mregexp -p2 -t"<(\w+)>.*(http://tinyurl.com/[^&\?\.]+)" check_tiny_chan = /quote -0 %P1 !~/bin/untiny.pl '%P2'
-#/def -mregexp -p2 -t"<(\w+)>.*(http://bit.ly/[^&\?\.]+)" check_bitly_chan = /quote -0 %P1 !~/bin/untiny.pl '%P2'
-#/def -mregexp -p2 -t"<(\w+)>.*(http://goo.gl/[^&\?\.]+)" check_googl_chan = /quote -0 %P1 !~/bin/untiny.pl '%P2'
-#/def -mregexp -p2 -t"<(\w+)>.*(http://www.youtube.com/watch\?v=[^&\?\.]+)" check_youtube_chan = /if (%P1 !~ "ichat") /quote -0 %P1 !~/bin/untiny.pl '%P2'%; /endif
-
-#/def -mregexp -p2 -t"\[(\w+)\].*(http://tinyurl.com/[^&\?\.]+)" check_tiny_chan = /if (%P1 =~ "imud_gossip") /quote -0 %P1 !~/bin/untiny.pl '%P2'%; /endif
-#/def -mregexp -p2 -t"\[(\w+)\].*(http://bit.ly/[^&\?\.]+)" check_bitly_chan = /if (%P1 =~ "imud_gossip") /quote -0 %P1 !~/bin/untiny.pl '%P2'%; /endif
-#/def -mregexp -p2 -t"\[(\w+)\].*(http://goo.gl/[^&\?\.]+)" check_googl_chan = /if (%P1 =~ "imud_gossip") /quote -0 %P1 !~/bin/untiny.pl '%P2'%; /endif
-#/def -mregexp -p2 -t"\[(\w+)\].*(http://www.youtube.com/watch\?v=[^&\?\.]+)" check_youtube_chan = /if (%P1 =~ "imud_gossip") /quote -0 %P1 !~/bin/untiny.pl '%P2'%; /endif
+#/def -mregexp -p2 -t"<(\w+)>.*(https?://tinyurl.com/[^&\?\.]+)" check_tiny_chan = /quote -0 url !~/bin/untiny.pl '%P2' '%P1'
+#/def -mregexp -p2 -t"<(\w+)>.*(https?://bit.ly/[^&\?\.]+)" check_bitly_chan = /quote -0 url !~/bin/untiny.pl '%P2' '%P1'
+#/def -mregexp -p2 -t"<(\w+)>.*(https?://goo.gl/[^&\?\.]+)" check_googl_chan = /quote -0 url !~/bin/untiny.pl '%P2' '%P1'
+#/def -mregexp -p2 -t"<(\w+)>.*(https?://mcaf.ee/[^&\?\.]+)" check_mcafee_chan = /quote -0 url !~/bin/untiny.pl '%P2' '%P1'
+#/def -mregexp -p2 -t"<(\w+)>.*(https?://migre.me/[^&\?\.]+)" check_migreme_chan = /quote -0 url !~/bin/untiny.pl '%P2' '%P1'
+#/def -mregexp -p2 -t"<(\w+)>.*(https?://durl.me/[^&\?\.]+)" check_durlme_chan = /quote -0 url !~/bin/untiny.pl '%P2' '%P1'
+#/def -mregexp -p2 -t"<(\w+)>.*(https?://is.gd/[^&\?\.]+)" check_isgd_chan = /quote -0 url !~/bin/untiny.pl '%P2' '%P1'
+#/def -mregexp -p2 -t"<(\w+)>.*(https?://youtu.be/[^&\?\.]+)" check_yout_chan = /quote -0 url !~/bin/untiny.pl '%P2' '%P1'
+#/def -mregexp -p2 -t"<(\w+)>.*(https?://www.youtube.com/watch\?v=[^&\?\.]+)" check_youtube_chan = /if (%P1 !~ "url") /quote -0 url !~/bin/untiny.pl '%P2' '%P1'%; /endif
 #
-# You can force posting on a particular channel by replacing the %P1 after /quote with a fixed channel string.
-#
-#/def -mregexp -p2 -t"<(\w+)>.*(https?://tinyurl.com/[^&\?\.]+)" check_tiny_chan = /quote -0 url !~/bin/untiny.pl '%P2'
-#/def -mregexp -p2 -t"<(\w+)>.*(https?://bit.ly/[^&\?\.]+)" check_bitly_chan = /quote -0 url !~/bin/untiny.pl '%P2'
-#/def -mregexp -p2 -t"<(\w+)>.*(https?://goo.gl/[^&\?\.]+)" check_googl_chan = /quote -0 url !~/bin/untiny.pl '%P2'
-#/def -mregexp -p2 -t"<(\w+)>.*(https?://www.youtube.com/watch\?v=[^&\?\.]+)" check_youtube_chan = /if (%P1 !~ "url") /quote -0 url !~/bin/untiny.pl '%P2'%; /endif
-
 
 use strict;
 use English;
 use Data::Dumper;
 use HTTP::Request::Common qw(POST);
+use HTML::Entities;
 use LWP::UserAgent;
 use URI;
+
+my $CHATTER = '/home/bloodlines/lib/secure/save/chat.o';
+
+sub channel_color {
+    my $channel = shift;
+    my %colors = (
+        "intermud"    => "%^B_BLACK%^%^GREY%^",
+        "muds"        => "%^B_BLACK%^%^GREY%^",
+        "connections" => "%^B_BLACK%^%^WHITE%^",
+        "death"       => "%^LIGHTRED%^",
+        "cre"         => "%^LIGHTGREEN%^",
+        "admin"       => "%^LIGHTMAGENTA%^",
+        "newbie"      => "%^B_YELLOW%^%^BLACK%^",
+        "gossip"      => "%^B_BLUE%^%^YELLOW%^",
+
+        "ds"          => "%^YELLOW%^",
+        "dchat"	      => "%^CYAN%^",
+        "intergossip" => "%^GREEN%^",
+        "intercre"    => "%^ORANGE%^",
+
+        "ibuild"      => "%^B_RED%^%^YELLOW%^",
+        "ichat"       => "%^B_RED%^%^GREEN%^",
+        "mbchat"      => "%^B_RED%^%^GREEN%^",
+        "pchat"       => "%^B_RED%^%^LIGHTGREEN%^",
+        "i2game"      => "%^B_BLUE%^",
+        "i2chat"      => "%^B_GREEN%^",
+        "i3chat"      => "%^B_RED%^",
+        "i2code"      => "%^B_YELLOW%^%^RED%^",
+        "i2news"      => "%^B_YELLOW%^%^BLUE%^",
+        "imudnews"    => "%^B_YELLOW%^%^CYAN%^",
+        "irc"         => "%^B_BLUE%^%^GREEN%^",
+        "ifree"       => "%^B_BLUE%^%^GREEN%^",
+
+        "default"      => "%^LIGHTBLUE%^",
+        "default-IMC2" => "%^B_BLUE%^%^WHITE%^"
+    );
+    return $colors{default} if !defined $channel;
+    return $colors{$channel} if exists $colors{$channel};
+    return $colors{default};
+}
 
 sub get_url {
     my $url = shift;
@@ -56,8 +92,8 @@ sub get_url {
     my $given_host = $given_uri->host;
     my $origin_uri = undef;
 
-    #print "DEBUG: given URL:  $given_uri\n";
-    #print "DEBUG: given HOST: $given_host\n";
+    #print STDERR "DEBUG: given URL:  $given_uri\n";
+    #print STDERR "DEBUG: given HOST: $given_host\n";
 
     if($request) {
         eval {
@@ -72,11 +108,11 @@ sub get_url {
             my $origin = undef;
             for( my $prev = $response; defined $prev; $prev = $prev->previous ) {
                 $origin = $prev->header("location");
-                #print "DEBUG: $origin\n";
-                #print "DEBUG: " . Dumper($prev) . "\n";
+                #print STDERR "DEBUG: $origin\n";
+                #print STDERR "DEBUG: " . Dumper($prev) . "\n";
                 last if defined $origin;
             }
-            #print "DEBUG: origin URL: $origin\n" if defined $origin;
+            #print STDERR "DEBUG: origin URL: $origin\n" if defined $origin;
             $origin_uri = (defined $origin) ? URI->new($origin) : $given_uri->clone;
         }
         return ($origin_uri, $response->content);
@@ -97,6 +133,7 @@ sub get_youtube_title {
 
     $page =~ /<meta\s+name=\"title\"\s+content=\"([^\"]*)\">/;
     my ($title) =  ($1);
+    $title = decode_entities($title) if defined $title;
     return $title;
 }
 
@@ -105,6 +142,7 @@ sub get_youtube_desc {
 
     $page =~ /<meta\s+name=\"description\"\s+content=\"([^\"]*)\">/;
     my ($desc) =  ($1);
+    $desc = decode_entities($desc) if defined $desc;
     return $desc;
 }
 
@@ -116,25 +154,45 @@ sub get_youtube_keywords {
     return \@keywords;
 }
 
+sub get_youtube_duration {
+    my $page = shift;
+
+    $page =~ /<meta\s+itemprop=\"duration\"\s+content=\"([^\"]*)\">/;
+    my ($funky) = ($1);
+    $funky =~ /.*?(\d+)M(\d+)S/;
+    my ($minutes, $seconds) = ($1, $2);
+    return sprintf "%d:%02d", $minutes, $seconds;
+}
+
 my $url = shift;
 my $given_uri = URI->new($url);
 my $given_host = $given_uri->host;
 
 my $channel = shift;
-$channel = " from ($channel)" if defined $channel;
-$channel = "" if !defined $channel;
 my ($origin, $page) = get_url($url);
-#print "DEBUG: $page\n";
+#print STDERR "DEBUG: $page\n";
 
 my $youtube_id = get_youtube_id($page) if defined $page;
 my $youtube_title = get_youtube_title($page) if defined $youtube_id;
+my $youtube_duration = get_youtube_duration($page) if defined $youtube_id;
+my $chan_color = channel_color($channel) if defined $channel;
+
 #my $youtube_desc = get_youtube_desc($page);
 #my $youtube_keywords = get_youtube_keywords($page);
 
-if (defined $youtube_id and defined $youtube_title) {
-    print "YouTube [$youtube_id]$channel is $youtube_title\n";
+$channel = " from $chan_color<$channel>%^RESET%^" if defined $channel and defined $chan_color;
+$channel = " from <$channel>" if defined $channel and !defined $chan_color;
+$channel = "" if !defined $channel;
+
+$youtube_id = "%^YELLOW%^[$youtube_id]%^RESET%^" if defined $youtube_id;
+$youtube_duration = " %^RED%^($youtube_duration)%^RESET%^" if defined $youtube_duration;
+
+if (defined $youtube_id and defined $youtube_title and defined $youtube_duration) {
+    print "YouTube $youtube_id$channel is $youtube_title$youtube_duration\n";
+} elsif (defined $youtube_id and defined $youtube_title) {
+    print "YouTube $youtube_id$channel is $youtube_title\n";
 } elsif (defined $origin) {
-    #print "DEBUG: " . Dumper($origin) . "\n";
+    #print STDERR "DEBUG: " . Dumper($origin) . "\n";
     print $given_host . " URL$channel goes to " . $origin->host . "\n";
 }
 
